39a5f62954f09f1c45833367bf1d157ad3fc539e
Kiran Gouda <lipigouda2004@gmail.com>
2025-10-27 12:46:27 +0530

    Add files via upload

--- BankingMainProject.py
+++ BankingMainProject.py
@@ -1,25 +1,34 @@
 from BankMenu import menu
 from CustomerAdd import addcustomer
-from CustomerPinUpdate import pinupdate
-from CustomerView import viewallcustomer, viewcustomer
-from Withdraw import withdraw
-menu()
-ch=int(input("Enter Ur choice:"))
-match(ch):
-    case 1:
-        addcustomer()
-    case 2:pass
-    case 3:
-        withdraw()
-    case 4:pass
-    case 5:
-        viewcustomer()
-    case 6:
-        viewallcustomer()
-    case 7:pass
-    case 8:
-        #pinupdate()
-    case 9:
-        print("\tThnx For using this Project")
-    case _:
-        print("\tUr selection of choice is wrong--try again")
+from CustomerDelete import deleteemployee
+from CustomerPinUpdate import updatepin
+from CustomerSearch import searchcustomer
+from CustomerView import viewcustomer, viewallcustomer
+from CustomerUpdate import withdraw, deposit
+while(True):
+    try:
+        menu()
+        ch=int(input("Enter Ur choice:"))
+        match(ch):
+            case 1:
+                addcustomer()
+            case 2:
+                deleteemployee()
+            case 3:
+                withdraw()
+            case 4:
+                deposit()
+            case 5:
+                viewcustomer()
+            case 6:
+                viewallcustomer()
+            case 7:
+                searchcustomer()
+            case 8:
+                updatepin()
+            case 9:
+                print("\tThnx For using this Project")
+            case _:
+                print("\tUr selection of choice is wrong--try again")
+    except ValueError:
+        print("Don't Enter Alnums ,strs,symbols for ur choice")
